b5233ccd71e6fd1d9397ec98c313b4ece4970f4c
Srimayee Bhattacharyya <138964786+srimayeebhattacharyya@users.noreply.github.com>
2026-04-20 19:42:34 +0530

    Delete SILVERSKY directory

--- SILVERSKY/DAY 4/Login Validation.js
+++
@@ -1,8 +0,0 @@
-let username = "admin";
-let password = "password123";
-
-if (username === "admin" && password === "password123") {
-  console.log("Login successful!");
-} else {
-  console.log("Invalid username or password.");
-}
